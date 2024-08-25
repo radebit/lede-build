2611d12a735537a22df051e144896dfb500551fd
Rade <26564356@qq.com>
2024-08-25 20:50:19 +0800

    feat: upgrade immortalwrt

--- preset-immortalwrt/1.clone.sh
+++ preset-immortalwrt/1.clone.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # download base code
 CODE_DIR=_firmware_code
-git clone --single-branch -b openwrt-21.02 https://github.com/immortalwrt/immortalwrt.git $CODE_DIR
+CODE_URL=https://github.com/immortalwrt/immortalwrt.git
+CODE_BRANCH=openwrt-23.05
+SWITCH_LATEST_TAG=false
+git clone --single-branch -b $CODE_BRANCH $CODE_URL $CODE_DIR
+if $SWITCH_LATEST_TAG; then
+    cd $CODE_DIR
+    LATEST_TAG_HASH=$(git rev-list --tags --max-count=1)
+    if [ -z "$LATEST_TAG_HASH" ]; then
+        echo "No tag to switch, keep the latest commit"
+    else
+        git checkout $LATEST_TAG_HASH
+        LATEST_TAG=$(git describe --tags $LATEST_TAG_HASH)
+        echo "The code has been switched to the latest stable version $LATEST_TAG"
+    fi
+    cd ..
+fi
 mv ./$CODE_DIR/* ./
 
 # download app codes
@@ -12,5 +27,5 @@ mkdir $SUPPLY_DIR && cd $SUPPLY_DIR
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
