77cf88a53f533f330d1e839ca858220068bc0df9
Rade <26564356@qq.com>
2024-12-22 16:25:46 +0800

    feat: upgrade preset-x-wrt

--- preset-x-wrt/1.clone.sh
+++ preset-x-wrt/1.clone.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # download base code
 CODE_DIR=_firmware_code
-SWITCH_TAG_FLAG=false
+SWITCH_LATEST_TAG=false
 git clone https://github.com/x-wrt/x-wrt.git $CODE_DIR
-if $SWITCH_TAG_FLAG; then
+if $SWITCH_LATEST_TAG; then
     cd $CODE_DIR
-    LATEST_TAG=$(git tag --sort=creatordate | awk 'END {print}')
-    git checkout $LATEST_TAG
-    echo "已切换到最近稳定版本$LATEST_TAG"
+    LATEST_TAG_HASH=$(git rev-list --tags --max-count=1)
+    if [ -z "$LATEST_TAG_HASH" ]; then
+        echo "No tag to switch, keep the latest commit"
+    else
+        git checkout $LATEST_TAG_HASH
+        LATEST_TAG=$(git describe --tags $LATEST_TAG_HASH)
+        echo "The code has been switched to the latest stable version $LATEST_TAG"
+    fi
     cd ..
 fi
 mv ./$CODE_DIR/* ./
@@ -21,6 +26,6 @@ git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
 git clone --depth 1 https://github.com/yichya/luci-app-xray.git
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git pw-dependencies
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall.git && mv openwrt-passwall/luci-app-passwall ./ && rm -rf openwrt-passwall
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git && mv openwrt-passwall2/luci-app-passwall2 ./ && rm -rf openwrt-passwall2
+git clone --depth 1 https://github.com/vernesong/OpenClash.git && mv OpenClash/luci-app-openclash ./ && rm -rf OpenClash
